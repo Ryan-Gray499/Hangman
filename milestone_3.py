64088f524fd49c6fb231759cde04609e2b37e96b
Ryan-Gray499 <n0982160@my.ntu.ac.uk>
2024-10-01 15:24:34 +0100

    create functions to get input from user and check the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -6,20 +6,45 @@ word_list = ["Bananna", "Satsuma", "Pear", "Strawberry", "Apple"]
 
 # get random word from the list
 random_word = random.choice(word_list)
+random_word = random_word.lower()
 print(random_word)
 
 
 # take a guess from user and check it is valid input
-while True:
-    guess = input("Please guess a letter: ")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+#while True:
+    #guess = input("Please guess a letter: ")
+    #if len(guess) == 1 and guess.isalpha() == True:
+        #break
+    #else:
+        #print("Invalid letter. Please, enter a single alphabetical character.")
 
 
 # check if guess is in word
-if guess in random_word:
-    print(f"Good guess, {guess} is in word")
-else:
-    print(f"sorry, {guess} is not in the word")
+#if guess in random_word:
+    #print(f"Good guess, {guess} is in word")
+#else:
+    #print(f"sorry, {guess} is not in the word")
+
+
+# Create function to check if guess is in the word
+
+def check_guess(guess):
+    guess.lower()
+    if guess in random_word:
+        print(f"Good guess, {guess} is in word")
+    else:
+        print(f"sorry, {guess} is not in the word")
+
+
+# create function that asks user for an input
+
+def ask_for_input():
+    while True:
+        guessed_letter = input("Please guess a letter: ")
+        if len(guessed_letter) == 1 and guessed_letter.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guessed_letter)
+
+ask_for_input()
